d9c030fb5c6ad0d0bd8697c211648172c43c4020
daianaddiaz <zirconico@gmail.com>
2026-07-05 20:43:36 -0300

    agregado de fondo mas assets

--- js/juego.js
+++ js/juego.js
@@ -41,12 +41,41 @@ async function cargarAssets() {
     console.log("Cargando Assets...");
 
     try {
-        PIXI.Assets.add({alias: 'torreEstatica', src:"../assets/maquina de curacion.json"})
-        PIXI.Assets.add({alias: 'torreCurando', src:"../assets/maquina de curacion - heal.json"})
-        PIXI.Assets.add({alias: 'torreCooldown', src:"../assets/maquina de curacion cooldown.json"})
-        PIXI.Assets.add({alias: 'bacteriaVerdeDireccion', src:"../assets/Salto derecha abajo celeste.json"})
+        PIXI.Assets.add({alias: 'torreEstatica', src:"../assets/maquina de curacion.json"});
+        PIXI.Assets.add({alias: 'torreCurando', src:"../assets/maquina de curacion - heal.json"});
+        PIXI.Assets.add({alias: 'torreCooldown', src:"../assets/maquina de curacion cooldown.json"});
+        PIXI.Assets.add({alias: 'bacteriaSana', src:"../assets/bacteriaSana.json"});
+        PIXI.Assets.add({alias: 'fondo', src:"../assets/placa_petri.png"});
+        
+
+        const recursos = await PIXI.Assets.load(['fondo']);
+
+        texturaFondo = recursos.fondo 
+
+        const fondoSprite = new PIXI.Sprite(texturaFondo);
+        fondoSprite.anchor.set(0.5);
+        fondoSprite.x = app.screen.width / 2;
+        fondoSprite.y = app.screen.height / 2;
+
+
+
+        app.stage.addChild(fondoSprite);
+
+        const nombreAnimCelula = Object.keys(recursos.spritesheetCelulas.animations)[0];
+        animacionCelularSana = recursos.bacteriaSana.animations[nombreAnimCelula];
+
+        const nombreAnimVirus = Object.keys(recursos.spritesheetVirus.animations)[0];
+        animacionVirus = recursos.spritesheetVirus.animations[nombreAnimVirus];
+
+        window.addEventListener('resize', () => {
+            fondoSprite.x = app.screen.width / 2;
+            fondoSprite.y = app.screen.height / 2;
+        });
+
+    }catch (error) {
+        console.error("Error cargando los assets del juego:", error);
     }
 }
 
-
-window.onload = iniciarJuego;       
+// Arrancar el juego cuando cargue la página
+window.onload = iniciarJuego;
